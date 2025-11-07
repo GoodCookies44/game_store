9e73119b4ebfd21ad5d610722fb4c14276bdb17b
Aleksandr Chernov <sashacool75@gmail.com>
2025-11-07 10:09:01 +0300

    add loader for css modules

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,30 +1,33 @@
 // Modules
-import React from 'react'
-import { Title, Tabs } from '@mantine/core'
+import React from "react";
+import { Title, Tabs } from "@mantine/core";
 // Components
 // Styles
-import * as classes from './Header.module.css';
-
+import * as classes from "./Header.module.css";
 
 export default function Header() {
   return (
-  <header>
-    <section className={classes.header__disclaimer}>
-      <Title order={2}>Этот сайт создан с использованием API <a href='https://rawg.io/' 
-        target="_blank" 
-        rel='nofollow noopener noreferrer'>RAWG.io</a>
-      </Title>
-    </section>
-    <section>
-    <Tabs defaultValue="Новинки" >
-      <Tabs.List>
-        <Tabs.Tab value='Новинки'>Новинки</Tabs.Tab>
-        <Tabs.Tab value='Игры года'>Игры года</Tabs.Tab>
-        <Tabs.Tab value='Новинки'>Новинки</Tabs.Tab>
-      </Tabs.List>
-    </Tabs>
-    </section>
-  </header>  
-  )
+    <header>
+      <section className={classes.header__disclaimer}>
+        <Title order={2}>
+          Этот сайт создан с использованием API{" "}
+          <a
+            href="https://rawg.io/"
+            target="_blank"
+            rel="nofollow noopener noreferrer"
+          >
+            RAWG.io
+          </a>
+        </Title>
+      </section>
+      <section>
+        <Tabs defaultValue="Новинки">
+          <Tabs.List>
+            <Tabs.Tab value="Новинки">Новинки</Tabs.Tab>
+            <Tabs.Tab value="Игры года">Игры года</Tabs.Tab>
+          </Tabs.List>
+        </Tabs>
+      </section>
+    </header>
+  );
 }
-
