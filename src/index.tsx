315baea54b9e89551584580b089b19410507b4ff
Aleksandr Chernov <sashacool75@gmail.com>
2025-11-07 10:15:51 +0300

    add resolution for css

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,21 @@
 // Modules
 import React from "react";
-import {createRoot} from 'react-dom/client'
-import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClient } from './lib/react-query'; 
+import { createRoot } from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 // Components
 import { MantineThemeProvider } from "./lib/mantin-theme";
-import App from './App'
+import App from "./App";
 // Styles
-import '@mantine/core/styles.css'
+import "@mantine/core/styles.css";
 
+const container = document.getElementById("root");
+const root = createRoot(container!);
 
-const container = document.getElementById('root')
-const root = createRoot(container!)
-
-
-root.render(  
-<QueryClientProvider client={queryClient}>
-  <MantineThemeProvider>
-    <App/>
-  </MantineThemeProvider>
-</QueryClientProvider>
-)
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <MantineThemeProvider>
+      <App />
+    </MantineThemeProvider>
+  </QueryClientProvider>
+);
